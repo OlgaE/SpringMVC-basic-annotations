2d70380773d0cc1bef2da05985df283313839718
OlgaE <olga.efimova.aol@gmail.com>
2013-12-28 11:56:38 +0700

    Spring Interceptor for a particular url.)

---
+++ SpringMVC4/src/main/java/com/demo/interceptors/InterceptorTest2.java
@@ -0,0 +1,29 @@
+package com.demo.interceptors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class InterceptorTest2 implements HandlerInterceptor {
+
+	public void afterCompletion(HttpServletRequest arg0,
+			HttpServletResponse arg1, Object arg2, Exception arg3)
+			throws Exception {
+		
+	}
+
+	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1,
+			Object arg2, ModelAndView arg3) throws Exception {
+		
+	}
+
+	public boolean preHandle(HttpServletRequest arg0, HttpServletResponse arg1,
+			Object arg2) throws Exception {
+		System.out.println("This is another interceptor)");
+        
+        return true;
+	}
+
+}
